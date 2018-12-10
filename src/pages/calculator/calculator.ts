2a8647f553b2ef0c363ffb267e0e59f4357f1420
William Schneiderman <william.m.schneiderman@gmail.com>
2018-12-10 15:23:57 +0100

    adds calculations to calculator.ts, adds message return to calculator.ts and adds labes and calculate button to calculator.html

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -15,6 +15,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CalculatorPage {
 
+  height: number;
+  weight: number;
+  bmiValue: number;
+  bmiMessage: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
@@ -22,4 +27,28 @@ export class CalculatorPage {
     console.log('ionViewDidLoad CalculatorPage');
   }
 
+  calculateBMI() {
+    if (this.weight > 0 && this.height > 0) {
+      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+  }
+  private setBMIMessage() {
+    if (this.bmiValue < 18.5) {
+      this.bmiMessage = "Underweight"
+    }
+  
+    if (this.bmiValue > 18.5 && this.bmiValue < 25) {
+      this.bmiMessage = "Normal"
+    }
+  
+    if (this.bmiValue > 25 && this.bmiValue < 30) {
+      this.bmiMessage = "Overweight"
+    }
+  
+    if (this.bmiValue > 30) {
+      this.bmiMessage = "Obese"
+    }
+   }
 }
